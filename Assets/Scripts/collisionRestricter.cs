b20e7d21005d1469eab63da1cc9b7662fb619c93
Michal Fibich <Michal.Fibichidis@gmail.com>
2017-12-02 03:10:25 +0100

    - Left and Right invisible walls can ignore all objects on "Enemies" layer

---
+++ Assets/Scripts/collisionRestricter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class collisionRestricter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Physics2D.IgnoreLayerCollision(8, 9, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
