b73a3a9d606a42cc891bf359aad66b5bf1aa1681
Michal Fibich <Michal.Fibichidis@gmail.com>
2017-12-02 08:45:24 +0100

    - Projectile added to the game - Enemy shoots projectiles

---
+++ Assets/Scripts/projectileShooter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectileShooter : MonoBehaviour
+{
+    public GameObject projectile;
+
+    public float shootingInterval;
+    public float shootingPower;
+    private float lastShotTime;
+
+    private Vector2 shootingDirection = new Vector2(-1, 0);
+
+    // Use this for initialization
+    void Start()
+    {
+        lastShotTime = shootingInterval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float delta = Time.deltaTime;
+        lastShotTime -= delta;
+
+        if (lastShotTime <= 0)
+        {
+            lastShotTime = shootingInterval + lastShotTime;
+            Shoot();
+        }
+
+    }
+
+    private void Shoot()
+    {
+        GameObject _projectile = Instantiate(projectile);
+        _projectile.transform.position = transform.position;
+        _projectile.GetComponent<Rigidbody2D>().AddForce(shootingDirection * shootingPower);
+
+        //projectile _projectileScript = _projectile.GetComponent<projectile>();
+        //_projectileScript.speed = gameObject.GetComponent<enemyMovement_1>().speed * 3;
+        //_projectileScript.movementDirection = new Vector2(-1, 0);
+    }
+}
