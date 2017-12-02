80da34b1a55a610c841134f26999c139ea0c1d75
Michal Fibich <Michal.Fibichidis@gmail.com>
2017-12-02 11:14:13 +0100

    - Projectiles now get reflected by pressing Space - Projectiles face away from player upon getting reflected

---
+++ Assets/Scripts/projectileReflecter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class projectileReflecter : MonoBehaviour
+{
+
+    public float influenceRadius;
+    public float reflectPower;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            ReflectProjectiles();
+    }
+
+    void ReflectProjectiles()
+    {
+        Vector2 reflectionCenter = gameObject.transform.position;
+
+        var projectiles = Physics2D.OverlapCircleAll(reflectionCenter, influenceRadius)
+            .Where(z => z.gameObject.layer == 10).ToArray();
+
+        foreach (var projectile in projectiles)
+        {
+            Vector2 reflectDirection = projectile.transform.position - transform.position;
+
+            Rigidbody2D rb2d = projectile.GetComponent<Rigidbody2D>();
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(reflectDirection * reflectPower);
+
+            projectileRotator prota = projectile.GetComponent<projectileRotator>();
+            prota.FaceAwayFromDirection(transform);
+        }
+    }
+}
---
+++ Assets/Scripts/projectileRotator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectileRotator : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    float magicConstant = 10000f;
+    public void FaceAwayFromDirection(Transform faceAwayFrom)
+    {
+        Vector3 vectorToTarget = transform.position - faceAwayFrom.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, magicConstant);
+    }
+}
