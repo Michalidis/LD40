6ab2085469374ab54af5c7e9576f142bcf4eebe0
Michal Fibich <Michal.Fibichidis@gmail.com>
2017-12-02 09:04:12 +0100

    - EnemyProjectiles layer created - Projectiles ignore collisions with - Projectiles, enemies and invisible walls

--- Assets/Scripts/collisionRestricter.cs
+++ Assets/Scripts/collisionRestricter.cs
@@ -1,14 +1,20 @@
 ﻿using UnityEngine;
 
-public class collisionRestricter : MonoBehaviour {
+public class collisionRestricter : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-        Physics2D.IgnoreLayerCollision(8, 9, true);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        Physics2D.IgnoreLayerCollision(8, 9, true);     // Enemy Ships vs. Walls
+        Physics2D.IgnoreLayerCollision(10, 9, true);    // Enemy Projectiles vs. Enemies
+        Physics2D.IgnoreLayerCollision(8, 10, true);    // Walls vs. Enemy Projectiles
+        Physics2D.IgnoreLayerCollision(10, 10, true);   // Enemy Projectiles vs. Enemy Projectiles
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
