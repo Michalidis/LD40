82a79ad441da00dec05753f1b3beeb2dc00f4ebe
Michal Fibich <Michal.Fibichidis@gmail.com>
2017-12-02 02:38:42 +0100

    - Player has been added to the scene - Player movement control has been added

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody2D _rigidbody2D;
+    // Use this for initialization
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        _rigidbody2D.MovePosition(_rigidbody2D.position + movement * speed);
+    }
+}
