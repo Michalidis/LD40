47ea6ece7a8f818540bbdf5237f9a46053996f77
Michal Fibich <Michal.Fibichidis@gmail.com>
2017-12-04 03:14:24 +0100

    - OnMouseEnter sounds added to buttons

---
+++ Assets/Scripts/Main Menu/OnMouseEnterScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnMouseEnterScript : MonoBehaviour, IPointerEnterHandler
+{
+    private MenuSounds ms;
+    // Use this for initialization
+    void Start()
+    {
+        ms = GameObject.Find("Sounds").GetComponent<MenuSounds>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ms.PlayEntered();
+    }
+}
