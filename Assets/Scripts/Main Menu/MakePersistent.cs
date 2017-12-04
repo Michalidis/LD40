aa7f86f4213b1daf2a2ece7adfb4b7d8899dde02
Michal Fibich <Michal.Fibichidis@gmail.com>
2017-12-04 12:58:30 +0100

    - Persistent Object is no longer created more than once

--- Assets/Scripts/Main Menu/MakePersistent.cs
+++ Assets/Scripts/Main Menu/MakePersistent.cs
@@ -1,16 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class MakePersistent : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class MakePersistent : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        GameObject[] existingInfo = GameObject.FindGameObjectsWithTag("GameController");
+        if (existingInfo.Length == 2)
+        {
+            GameObject.Find("SinglePlayer").GetComponent<CanvasToggler>().Toggle();
+            return;
+        }
         DontDestroyOnLoad(gameObject);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
