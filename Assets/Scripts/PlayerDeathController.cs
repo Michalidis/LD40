c1fe79d0db50deaf79f52e5e6e02f0249e0aabc6
Michal Fibich <Michal.Fibichidis@gmail.com>
2017-12-04 12:46:51 +0100

    - Death Animation added to main character - Death causes main menu to load

---
+++ Assets/Scripts/PlayerDeathController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerDeathController : MonoBehaviour
+{
+
+    public ParticleSystem DeathParticleSystem;
+    // Use this for initialization
+    void Start()
+    {
+        DeathParticleSystem.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GetComponent<Animator>().SetBool("isDead", true);
+        DeathParticleSystem.Play();
+        DisableComponents();
+        StartCoroutine(DelayParticleStopAndLoadMenu());
+    }
+
+    void DisableComponents()
+    {
+        GetComponent<PlayerMovement>().enabled = false;
+        GetComponent<ProjectileReflecter>().enabled = false;
+        GetComponent<PolygonCollider2D>().enabled = false;
+        GetComponent<PlayerDeathController>().enabled = false;
+    }
+
+    IEnumerator DelayParticleStopAndLoadMenu()
+    {
+        yield return new WaitForSeconds(0.3f);
+        GetComponentInChildren<ParticleSystem>().Stop();
+        yield return new WaitForSeconds(1.7f);
+        GetComponent<SceneLoader>().LoadMenuScene();
+    }
+}
