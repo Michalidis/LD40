576c6304f8ec9d26f5b70f469f8d7f40d2ae8560
Michal Fibich <Michal.Fibichidis@gmail.com>
2017-12-04 19:39:08 +0100

    - Data Gathering for persistent info

--- Assets/Scripts/Main Menu/Upgrades.cs
+++ Assets/Scripts/Main Menu/Upgrades.cs
@@ -8,6 +8,7 @@ public class Upgrades : MonoBehaviour
     public float deflect_power;
     public float deflect_rate;
     public float deflect_radius;
+    public float movement_speed;
 
     // Use this for initialization
     void Start()
@@ -28,5 +29,21 @@ public class Upgrades : MonoBehaviour
         globalUpgrades.deflect_power = 0;
         globalUpgrades.deflect_radius = 0;
         globalUpgrades.deflect_rate = 1;
+        globalUpgrades.movement_speed = 0;
+    }
+
+    public void GetData()
+    {
+        Upgrades globalUpgrades = GameObject.Find("IndestructibleInfo").GetComponentInChildren<Upgrades>();
+
+        globalUpgrades.deflect_power = GetBonus("DeflectPower");
+        globalUpgrades.deflect_radius = GetBonus("DeflectRadius");
+        globalUpgrades.deflect_rate = GetBonus("DeflectRate");
+        globalUpgrades.movement_speed = GetBonus("FlySpeed");
+    }
+
+    private float GetBonus(string fromObjectName)
+    {
+        return GameObject.Find(fromObjectName).GetComponent<UpgradeToolTip>().CurrentBonus;
     }
 }
