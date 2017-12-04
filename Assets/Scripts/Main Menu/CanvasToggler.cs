065b7ac025d14bdf69759b78bf5cfab6c23fbe5f
Michal Fibich <Michal.Fibichidis@gmail.com>
2017-12-04 01:53:13 +0100

    - Main Menu Scene created - Basic Button Navigation added

---
+++ Assets/Scripts/Main Menu/CanvasToggler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CanvasToggler : MonoBehaviour {
+
+    public Canvas Current;
+    public Canvas Target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Toggle()
+    {
+        Current.enabled = !Current.enabled;
+        Target.enabled = !Target.enabled;
+    }
+}
